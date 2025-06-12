ebf9e974071ddbdfdec41c6ec2696b69c5daf3b9
mhmdazkanfl <mhmdazkanfl@gmail.com>
2025-06-12 22:38:47 +0700

    docs: delete unecessary log

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,7 +7,6 @@ import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const token = event.cookies.get('session') ?? null;
-	console.log(`Middleware (token): ${token}`);
 	if (token === null) {
 		event.locals.user = null;
 		event.locals.session = null;
@@ -23,7 +22,5 @@ export const handle: Handle = async ({ event, resolve }) => {
 
 	event.locals.session = tempSession;
 	event.locals.user = tempUser;
-	console.log(`Middleware (user id): ${event.locals.user?.id}`);
-	console.log(`Middleware (session id): ${event.locals.session?.id}`);
 	return resolve(event);
 };
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,8 +2,6 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = ({ locals }) => {
-	console.log(`Root: ${locals.user}`);
-	console.log(`Root: ${locals.session}`);
 	if (locals.session || locals.user) return redirect(302, '/app');
 	return {};
 };
