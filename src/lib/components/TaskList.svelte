677094d08694d3497f789726d6430e809872d572
mhmdazkanfl <mhmdazkanfl@gmail.com>
2025-06-13 21:19:16 +0700

    feat: added add task form ui and task list ui

---
+++ src/lib/components/TaskList.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { ScrollArea } from './ui/scroll-area';
+	import { Separator } from './ui/separator';
+
+	const tags = Array.from({ length: 50 }).map((_, i, a) => `v1.2.0-beta.${a.length - i}`);
+</script>
+
+<ScrollArea class="h-72 w-full rounded-md border">
+	<div class="p-4">
+		{#each tags as tag (tag)}
+			<div class="text-sm">
+				{tag}
+			</div>
+			<Separator class="my-2" />
+		{/each}
+	</div>
+</ScrollArea>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,72 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button/index.js';
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
+	import { Textarea } from '$lib/components/ui/textarea';
+	import { Badge } from '$lib/components/ui/badge';
+	import * as Tabs from '$lib/components/ui/tabs';
+	import * as Card from '$lib/components/ui/card';
+	import Search from '@lucide/svelte/icons/search';
 	import { enhance } from '$app/forms';
+	import TaskList from '$lib/components/TaskList.svelte';
 </script>
 
-<form method="POST" action="?/logout" use:enhance>
+<form class="absolute top-4 right-16" method="POST" action="?/logout" use:enhance>
 	<Button type="submit">Log out</Button>
 </form>
+
+<div class="max-h-9/10 w-full max-w-sm md:max-w-xl">
+	<Card.Root class="center mb-4 w-full max-w-sm md:max-w-xl">
+		<Card.Header>
+			<Card.Title class="text-center">Add new task</Card.Title>
+		</Card.Header>
+		<Card.Content>
+			<form id="add-form" action="?/add" method="post" use:enhance>
+				<Input class="mb-2" placeholder="Title" />
+				<Textarea placeholder="Description"></Textarea>
+			</form>
+		</Card.Content>
+		<Card.Footer>
+			<Button class="mr-2">Add</Button>
+			<Button variant="destructive">Cancel</Button>
+		</Card.Footer>
+	</Card.Root>
+
+	<div class="center flex w-full max-w-sm flex-col gap-6 md:max-w-xl">
+		<Tabs.Root value="all">
+			<Tabs.List>
+				<Tabs.Trigger value="all">
+					All
+					<Badge class="h-5 min-w-5 rounded-full px-1 font-mono tabular-nums">12</Badge>
+				</Tabs.Trigger>
+				<Tabs.Trigger value="active">
+					Active
+					<Badge
+						class="h-5 min-w-5 rounded-full bg-blue-600 px-1 font-mono text-white tabular-nums dark:bg-blue-500 "
+						>10
+					</Badge>
+				</Tabs.Trigger>
+				<Tabs.Trigger value="completed">
+					Completed
+					<Badge variant="destructive" class="h-5 min-w-5 rounded-full px-1 font-mono tabular-nums">
+						2
+					</Badge>
+				</Tabs.Trigger>
+			</Tabs.List>
+
+			<div class="relative">
+				<Search class="text-muted-foreground absolute top-1/2 left-2.5 h-4 w-4 -translate-y-1/2" />
+				<Input placeholder="Filters" class="pl-8" />
+			</div>
+
+			<Tabs.Content value="all">
+				<TaskList></TaskList>
+			</Tabs.Content>
+			<Tabs.Content value="active">
+				<TaskList></TaskList>
+			</Tabs.Content>
+			<Tabs.Content value="completed">
+				<TaskList></TaskList>
+			</Tabs.Content>
+		</Tabs.Root>
+	</div>
+</div>
